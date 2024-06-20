1d4b5acd259c328bbb9b8d0cdbeda6806cc3246d
Sudeep <sudeepkarki74@gmail.com>
2024-06-20 11:57:54 +0545

    API Put for users/[id]

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -22,33 +22,69 @@ interface Props {
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
-  const body = await request.json();
-  const validation = schema.safeParse(body);
+  try {
+    const body = await request.json();
+    console.log("Request Body:", body);
 
-  if (!validation.success) {
-    return NextResponse.json(validation.error.errors, { status: 400 });
-  }
+    const validation = schema.safeParse(body);
+    if (!validation.success) {
+      console.error("Validation Error:", validation.error.errors);
+      return NextResponse.json(validation.error.errors, { status: 400 });
+    }
+    const user = await prisma.user.findUnique({
+      where: { id: parseInt(params.id) },
+    });
 
-  const user = await prisma.user.findUnique({
-    where: { id: params.id },
-  });
+    if (!user)
+      return NextResponse.json({ error: "User not found" }, { status: 401 });
 
-  if (!user) {
+    const updatedUser = {
+      ...user,
+      email: body.email,
+      name: body.name,
+    };
+
+    const savedUser = await prisma.user.update({
+      where: { id: user.id },
+      data: updatedUser,
+    });
+
+    return NextResponse.json(savedUser, { status: 200 });
+  } catch (error) {
     return NextResponse.json(
-      { message: "User to be edited doesn't exist" },
-      { status: 404 }
+      {
+        message: { error },
+      },
+      { status: 402 }
     );
   }
+  // if (!user) {
+  //   console.error("User Not Found:", params.id);
+  //   return NextResponse.json(
+  //     { message: "User to be edited doesn't exist" },
+  //     { status: 404 }
+  //   );
+  // }
 
-  const updatedUser = await prisma.user.update({
-    where: { id: params.id },
-    data: {
-      name: body.name,
-      email: body.email,
-    },
-  });
+  // if (user) return NextResponse.json(user, { status: 200 });
+  // return NextResponse.json({ error: "Error found" }, { status: 200 });
+  // const updatedUser = await prisma.user.update({
+  //   where: { id: params.id },
+  //   data: {
+  //     name: body.name,
+  //     email: body.email,
+  //   },
+  // });
 
-  return NextResponse.json(updatedUser, { status: 202 });
+  // console.log("Updated User:", updatedUser);
+  // return NextResponse.json(updatedUser, { status: 202 });
+  // } catch (error) {
+  //   console.error("Error in PUT request:", error);
+  //   return NextResponse.json(
+  //     { error: "Internal Server Error" },
+  //     { status: 500 }
+  //   );
+  // }
 }
